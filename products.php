8bc1f419b339dd7ae5aa7bc0dedd752a8ddcf396
Brenna <brennascholte@ucsb.edu>
2023-11-19 22:05:34 +0100

    Brenna - updated index.php and added products database and page, need to implement adding items to cart

--- products.php
+++ products.php
@@ -1,7 +1,7 @@
 <?php
     $con = mysqli_connect('localhost', 'root'); // connect to database
     mysqli_select_db($con, 'shop'); // select database
-    $sql = "SELECT * FROM products WHERE featured=1"; // select all products from database
+    $sql = "SELECT * FROM products"; // select all products from database
     $featured = mysqli_query($con, $sql); // query database
     // $featured = $con->query($sql); // other way to query database
 ?>
@@ -11,12 +11,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>Products</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel = "stylesheet" href="css/style.css">
 </head>
 <body>
+
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
@@ -28,34 +29,8 @@
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             </li>
-
-            <li class="nav-item">
-            <a class="nav-link" href="#signupform">Sign Up</a>
-            </li>
-
-            <li class="nav-item">
-            <a class="nav-link" href="#">Login In</a>
-            </li>
-
-            <!-- TODO: implement this button to direct to products.php -->
-            <li class="nav-item">
-            <a class="nav-link" href="#">Shop</a>
-            </li>
-
-            <!-- Optional Dropdown -->
-            <!-- <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-            </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            </li> -->
         </ul>
 
         <!-- Search Bar -->
@@ -67,13 +42,10 @@
     </div>
     </nav>
 
-    <!-- Home Page -->
-    <h2 class="text-center">Welcome to the Web Shop!</h2>
     <div class="col-md-2"> </div>
         <div class="text-center" class="col-md-8">
             <div class="row">
-                <h4 class="text-center">Top Products</h4>
-        
+                <h2 class="text-center">All Products</h2>
                 <?php
                     while($product = mysqli_fetch_assoc($featured)):
                 ?>
@@ -82,38 +54,16 @@
                         <h4> <?= $product['title']; ?> </h4> 
                         <img src="<?= $product["image"]; ?>" alt="<?= $product['title']; ?>" class="img-fluid pb-4" style="width:250px; height:200px; object-fit:cover;"/>
                         <p class="price">$<?= $product['price']; ?> </p>
+                        <p class="desc"><?= $product['description']; ?> </p>
+                        <p class="bname"><?= $product['brandname']; ?> </p>
                     </div>
+                    <!-- TODO: implement button to add item to cart -->
+                    <a href="cart.php">
+                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#item-1">Add to Cart</button>
+                    </a>
                 <?php endwhile; ?>
-                <a href="products.php">
-                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-1">View All Products</button>
-                </a>
             </div>
         </div>
-
-
-    <!-- Sign Up Form -->
-    <a id="signupform">
-        <section class="my-4">
-            <div class="py-4">
-                <h2 class="text-center">Sign Up</h2>
-            </div>
-
-            <div class="w-50 m-auto"> 
-                <form action="signup.php" method="post">
-                    <div class="form-group"> 
-                        <label>Username: </label>
-                        <input type="text" name="username" autocomplete="off" class="form-control">
-                    </div>
-                    <div class="form-group"> 
-                        <label>Password: </label>
-                        <input type="text" name="password" autocomplete="off" class="form-control">
-                    </div>
-                    <button type="submit" class="btn btn-success">Sign Up</button>
-                </form>
-            </div>
-        </section>
-    </a>
-
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"> </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"> </script>
