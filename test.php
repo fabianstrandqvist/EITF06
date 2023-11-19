13225d1119ef35a4ce84d2f276b6e72f9ef0dff5
Brenna <brennascholte@ucsb.edu>
2023-11-19 13:28:21 +0100

    Brenna - installed boostrap and updated test.php

--- test.php
+++ test.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Test Page</title>
 </head>
 <body>
-    
+    <!-- HTML Version -->
+    <h1>Welcome to Web Shop!</h1>
+    <p>Hello World</p>
+    <p>This is our test page!</p>
+
+    <!-- PHP Version-->
+    <?php echo '<h1>Welcome to Web Shop!</h1>';
+        echo '<p>Hello World</p>';    
+        echo '<p>This is our test page!</p>';
+    ?>
 </body>
 </html>
